cb8e13adad68b44617146ce7f593a37fe7e49c76
lochaok <lochaok@gmail.com>
2026-06-16 14:09:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 App tính tiền gửi tiết kiệm Việt Nam đề tài 1 Hà Minh Lộc")
 
 # Nhập dữ liệu
 C = st.number_input(
